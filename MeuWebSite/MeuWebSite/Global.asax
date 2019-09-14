8879030accc8c858f5fda6f83a18aa2618e4ea89
MIchAeL0L04 <semicollom@gmail.com>
2019-09-14 19:06:21 -0300

    projeto iniciado / erros responsivel

---
+++ MeuWebSite/MeuWebSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MeuWebSite.MvcApplication" Language="C#" %>
---
+++ MeuWebSite/MeuWebSite/Models/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using CompareAttribute = System.ComponentModel.DataAnnotations.CompareAttribute;
+
+namespace MeuWebSite.Models
+{
+    public class Pessoa
+    {
+        [Required(ErrorMessage = "O email é obrigatório")]
+        [RegularExpression(@"^[a-zA-Z]+(([\'\,\.\- ][a-zA-Z ])?[a-zA-Z]*)*\s+<(\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3})>$|^(\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3})$", ErrorMessage = "Digite um Email válido")]
+        public string Email { get; set; }
+    }
+}
