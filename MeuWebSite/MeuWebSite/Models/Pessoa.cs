99abcdc16696c6f8f29cf43f3ec4a39b0472d25a
MIchAeL0L04 <michael04descomplica@gmail.com>
2019-09-14 22:06:54 -0300

    Pequenas correções na TelaLogin

--- MeuWebSite/MeuWebSite/Models/Pessoa.cs
+++ MeuWebSite/MeuWebSite/Models/Pessoa.cs
@@ -13,5 +13,7 @@ namespace MeuWebSite.Models
         [Required(ErrorMessage = "O email é obrigatório")]
         [RegularExpression(@"^[a-zA-Z]+(([\'\,\.\- ][a-zA-Z ])?[a-zA-Z]*)*\s+<(\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3})>$|^(\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3})$", ErrorMessage = "Digite um Email válido")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "A senha é obrigatório")]
+        public string Senha { get; set; }
     }
 }
