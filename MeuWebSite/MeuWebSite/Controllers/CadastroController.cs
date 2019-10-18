15d0bfe41838d831c0ba4e569636080ee92a7eaf
2B Modular Info <2binfo@basilides.local>
2019-10-18 20:11:37 -0300

    Telas Cadastro

---
+++ MeuWebSite/MeuWebSite/Controllers/CadastroController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeuWebSite.Controllers
+{
+    public class CadastroController : Controller
+    {
+        // GET: Cadastro
+        public ActionResult Produto()
+        {
+            return View();
+        }
+
+        public ActionResult Fornecedor()
+        {
+            return View();
+        }
+
+        public ActionResult Estoque()
+        {
+            return View();
+        }
+
+        public ActionResult Cardapio()
+        {
+            return View();
+        }
+    }
+}
